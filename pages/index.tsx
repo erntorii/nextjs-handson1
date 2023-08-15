fa8a1f75ab00cd13dfe885e8d32b6ff7acfa0ec4
erntorii <erntorii@gmail.com>
2023-08-15 14:52:17 +0900

    feat: 個別のページで情報を取得と表示ができるようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 import dayjs from 'dayjs';
 import { useEffect } from 'react';
 import prism from 'prismjs';
+import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN
@@ -37,21 +38,41 @@ type StaticProps = {
   posts: Post[];
 };
 
-export const getPosts = async () => {
-  const database = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID || '',
-    filter: {
-      and: [
-        {
-          property: 'Published',
-          checkbox: { equals: true }
-        }
+export const getPosts = async (slug?: string) => {
+  let database: QueryDatabaseResponse | undefined = undefined;
+
+  if (slug) {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [
+          {
+            property: 'Slug',
+            rich_text: { equals: slug }
+          }
+        ]
+      }
+    });
+  } else {
+    database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || '',
+      filter: {
+        and: [
+          {
+            property: 'Published',
+            checkbox: { equals: true }
+          }
+        ]
+      },
+      sorts: [
+        { timestamp: 'created_time', direction: 'descending' }
       ]
-    },
-    sorts: [
-      { timestamp: 'created_time', direction: 'descending' }
-    ]
-  });
+    });
+  }
+
+  if (!database) {
+    return [];
+  }
 
   const posts: Post[] = [];
 
@@ -94,6 +115,73 @@ export const getPosts = async () => {
 
   return posts;
 };
+
+export const getPostContents = async (post: Post) => {
+  const blockResponse = await notion.blocks.children.list({
+    block_id: post.id
+  });
+  const contents: Content[] = [];
+
+  blockResponse.results.forEach((block) => {
+    if (!('type' in block)) {
+      return;
+    }
+
+    switch (block.type) {
+      case 'paragraph':
+        contents.push({
+          type: 'paragraph',
+          text: block.paragraph.rich_text[0]?.plain_text ?? null
+        });
+        break;
+
+      case 'heading_2':
+        contents.push({
+          type: 'heading_2',
+          text: block.heading_2.rich_text[0]?.plain_text ?? null
+        });
+        break;
+
+      case 'heading_3':
+        contents.push({
+          type: 'heading_3',
+          text: block.heading_3.rich_text[0]?.plain_text ?? null
+        });
+        break;
+
+      case 'quote':
+        contents.push({
+          type: 'quote',
+          text: block.quote.rich_text[0]?.plain_text ?? null
+        });
+        break;
+
+      case 'code':
+        contents.push({
+          type: 'code',
+          text: block.code.rich_text[0]?.plain_text ?? null,
+          language: block.code.language
+        });
+    }
+  });
+
+  return contents;
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async () => {
+  const posts = await getPosts();
+  const contentsList = await Promise.all(
+    posts.map((post) => {
+      return getPostContents(post);
+    })
+  );
+
+  posts.forEach((post, index) => {
+    post.contents = contentsList[index];
+  });
+
+  return { props: { posts } }
+}
 
 const Home: NextPage<StaticProps> = ({ posts }) => {
   useEffect(() => {
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,5 +1,5 @@
-import { GetStaticPaths } from "next";
-import { getPosts } from "..";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Post, getPostContents, getPosts } from "..";
 
 type StaticPathsParams = {
   slug: string;
@@ -21,3 +21,46 @@ export const getStaticPaths: GetStaticPaths<StaticPathsParams> = async () => {
 
   return { paths, fallback: 'blocking' };
 }
+
+type StaticProps = {
+  post?: Post;
+};
+
+export const getStaticProps: GetStaticProps<StaticProps, StaticPathsParams> = async ({
+  params,
+  preview
+}) => {
+  const notFoundProps = {
+    props: {},
+    redirect: {
+      destination: '/404'
+    }
+  };
+
+  if (!params) {
+    return notFoundProps;
+  }
+
+  const { slug } = params;
+  const posts = await getPosts(slug);
+  const post = posts.shift();
+
+  if (!post) {
+    return notFoundProps;
+  }
+
+  const contents = await getPostContents(post);
+  post.contents = contents;
+
+  return { props: { post } };
+};
+
+const PostPage: NextPage<StaticProps> = ({ post }) => {
+  if (!post) {
+    return null;
+  }
+
+  return <div>{JSON.stringify(post)}</div>;
+};
+
+export default PostPage;
